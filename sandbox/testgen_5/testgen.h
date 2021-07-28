eaac1e54489c421c6eea72773cd15dc79241b819
Ilia Zlatkin <ilya.zlatkin@gmail.com>
2021-07-28 14:11:12 -0400

    remove sandbox

--- sandbox/testgen_5/testgen.h
+++
@@ -1,8 +0,0 @@
-extern int nondet(void);
-int nondet_4904(){
- return 4904 + nondet();
-} 
-int nondet_4924(){
- return 4924 + nondet();
-} 
-
--- sandbox/testgen_9/testgen.h
+++
@@ -1,8 +0,0 @@
-extern int nondet(void);
-int nondet_4924(){
- return 4924 + nondet();
-} 
-int nondet_9413(){
- return 9413 + nondet();
-} 
-
