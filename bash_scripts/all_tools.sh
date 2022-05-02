5cd343023c4373384d4dd1ac3523d12a84f74778
izlatkin <zlatkin_ilya@mail.ru>
2022-05-02 12:55:24 -0400

    script to run all tools

---
+++ bash_scripts/all_tools.sh
@@ -0,0 +1,15 @@
+export IN_DIR=/home/fmfsu/Benchs/experiments
+export RESULT_DIR=/home/fmfsu/results/experiments
+
+python3.9 -u /home/fmfsu/PyCharm/launcher/VeriFuzzLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/verifuzz_sandbox &
+python3.9 -u /home/fmfsu/PyCharm/launcher/KleeLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/klee_sandbox &
+python3.9 -u /home/fmfsu/PyCharm/launcher/FuseBMCLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/fusebmc_sandbox &
+python3.9 -u /home/fmfsu/PyCharm/launcher/LibKluzzerLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/LibKluzzer_sandbox &
+python3.9 -u /home/fmfsu/PyCharm/launcher/CmaesfuzzLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/cmaesfuzz_sandbox &
+python3.9 -u /home/fmfsu/PyCharm/launcher/PrtestLauncher.py --input_source ${IN_DIR} \
+--output_dir ${RESULT_DIR}/prtest_sandbox &
