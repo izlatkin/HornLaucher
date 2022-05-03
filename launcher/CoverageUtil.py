5c444174dfbc4ac002605ecda77cb67d71040bde
izlatkin <zlatkin_ilya@mail.ru>
2022-05-02 21:01:42 -0400

    Bug fix for coverage merge algorithm

--- launcher/CoverageUtil.py
+++ launcher/CoverageUtil.py
@@ -26,8 +26,8 @@ class CoverageUtil:
                     else:
                         return 'merge error'
                 sf = 'SF:{}/{}\n'.format(path, key)
-                if not key.endswith('.h'): # ToDo removed when fixed https://github.com/izlatkin/HornLauncher/issues/12
-                    out += [sf] + CoverageUtil.merge_coverage_lists(tmp_list)
+                #if not key.endswith('.h'): # ToDo removed when fixed https://github.com/izlatkin/HornLauncher/issues/12
+                out += [sf] + CoverageUtil.merge_coverage_lists(tmp_list)
         return ['TN:\n'] + out
 
 
@@ -51,28 +51,29 @@ class CoverageUtil:
             else:
                 tmp = line.split(':')
                 tag = tmp[0]
-                data = tmp[1]
+                data = tmp[1].strip().split(',')
 
                 if tag == 'BRDA':
-                    c = data.split(',')[-1]
-                    if c == '1\n':
+                    c = data[-1]
+                    if c == '1':
                         branch_number += 1
                         result_list_of_lines.append(line)
                     else:
-                        flag = 0
-                        for j in covers[1:]:
-                            if j[index].split(',')[-1] == '1\n':
-                                branch_number += 1
-                                result_list_of_lines.append(j[index])
-                                flag = 1
-                                break
-                        if flag == 0:
+                        tmp_l = [j[index].strip().split(',')[-1] for j in covers[1:]]
+                        sum_coverage = 0
+                        for t in tmp_l:
+                            if is_int(t):
+                                sum_coverage += int(t)
+                        if sum_coverage > 0:
+                            #result_list_of_lines.append(covers[tmp_l.index('1') + 1][index])
+                            result_list_of_lines.append("BRDA:{},{}\n".format(','.join(data[:3]), sum_coverage))
+                        else:
                             result_list_of_lines.append(line)
                 elif tag == 'BRH':
                     result_list_of_lines.append("BRH:{}\n".format(branch_number))
                 elif tag == 'DA':
                     sum_coverage = 0
-                    lc = data.split(',')
+                    lc = data
                     if is_int(lc[-1]):
                         sum_coverage += int(lc[-1])
                     for j in covers[1:]:
@@ -145,7 +146,7 @@ def read_coverage_info(f):
             tmp = line.strip('\n').split(':')
             tag = tmp[0]
             data = tmp[1]
-            if tag == 'SF' and os.path.isfile(data):
+            if tag == 'SF': # and os.path.isfile(data):
                 tmp_file = os.path.basename(data)
             else:
                 tmp_content.append(line)
@@ -161,9 +162,29 @@ if __name__ == '__main__':
     # result = CoverageUtil.merge(['/tmp/sandbox/testgen_1/1/coverage.info',
     #                     '/tmp/sandbox/testgen_1/2/coverage.info',
     #                     '/tmp/sandbox/testgen_1/3/coverage.info'])
-    # result = CoverageUtil.merge_coverage_for_test_runs(['../sandbox/testgen_2/3/coverage.info',
-    #                                                     '../sandbox/testgen_2/6/coverage.info',
-    #                                                     '../sandbox/testgen_2/7/coverage.info'])
+    result = CoverageUtil.merge_coverage_for_test_runs(['../sandbox/test_8/3/coverage.info',
+                                                        '../sandbox/test_8/5/coverage.info',
+                                                        '../sandbox/test_8/1/coverage.info',
+                                                        '../sandbox/test_8/2/coverage.info',
+                                                        '../sandbox/test_8/4/coverage.info',
+                                                        '../sandbox/test_8/7/coverage.info',
+                                                        '../sandbox/test_8/6/coverage.info'])
+    # result = CoverageUtil.merge_coverage_for_test_runs(['/Users/ilyazlatkin/Downloads/sandbox2/test_11/1/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/2/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/3/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/4/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/5/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/6/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/7/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/8/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/9/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/10/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/11/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/12/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/13/coverage.info',
+    #                                                    '/Users/ilyazlatkin/Downloads/sandbox2/test_11/14/coverage.info',
+    #                                                     '/Users/ilyazlatkin/Downloads/sandbox2/test_11/15/coverage.info',
+    #                                                     '/Users/ilyazlatkin/Downloads/sandbox2/test_11/16/coverage.info',])
     # for i in result:
     #     print(i, end='')
     # result = CoverageUtil.merge_coverage_for_test_runs(
@@ -173,6 +194,9 @@ if __name__ == '__main__':
     # result = read_coverage_info('../sandbox/testgen_2/3/coverage.info')
     # result = read_coverage_info('../sandbox/testgen_2/6/coverage.info')
     # result = read_coverage_info('../sandbox/testgen_2/7/coverage.info')
+    summary_file = open('../sandbox/final_coverage_report/final_coverage.info', "w")
+    summary_file.writelines(result)
+    summary_file.close()
     result = CoverageUtil.remove_headers_coverage('../sandbox/final_coverage_report/final_coverage.info')
     for i in result:
         print(i, end='')
