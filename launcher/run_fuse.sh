1bac63c5e0315abdc87ddd82dc92a2fedc59fdb1
izlatkin <zlatkin_ilya@mail.ru>
2023-01-13 14:51:13 -0500

    add scripts for remote run

---
+++ launcher/run_fuse.sh
@@ -0,0 +1,5 @@
+#--input_source /home/fmfsu/Benchs/openssl-simplified_updated --docker_sea true --rerun flase
+python3.9 -u /home/fmfsu/PyCharm/launcher/FuseBMCLauncher.py --input_source \
+/home/fmfsu/Benchs/sv-benchmarks/c/array-memsafety \
+--output_dir \
+/home/fmfsu/results/array/array_fuse
---
+++ launcher/run_veri.sh
@@ -0,0 +1,4 @@
+python3.9 -u /home/fmfsu/PyCharm/launcher/VeriFuzzLauncher.py --input_source \
+/home/fmfsu/Benchs/sv-benchmarks/c/array-memsafety \
+--output_dir \
+/home/fmfsu/results/array/array_veri
